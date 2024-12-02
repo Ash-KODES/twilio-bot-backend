1749f7b2875a4c160017c8cb7ec68a5311c200e6
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2024-12-02 11:51:31 +0530

    Adding bucket name

--- firebase_handler.py
+++ firebase_handler.py
@@ -3,6 +3,6 @@ from firebase_admin import credentials, initialize_app, firestore, storage
 # Firebase Initialization
 cred = credentials.Certificate('./service.json')
 print(cred)
-app = initialize_app(cred, {"storageBucket": "your-bucket-name.appspot.com"})
+app = initialize_app(cred, {"storageBucket": "insait-realtime-api.firebasestorage.app"})
 db = firestore.client()
 bucket = storage.bucket()
